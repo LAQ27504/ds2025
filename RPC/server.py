f6a7b4adfa421f740781a845b02c17d0ea3fc735
LAQ27504 <anhquangcsp@gmail.com>
2024-12-02 21:13:33 +0700

    Quang: Update file and report

--- RPC/server.py
+++ RPC/server.py
@@ -12,20 +12,20 @@ def download(fileName):
 def upload(fileName : str, content : bytes):
     with open(fileName, "wb") as f:
         f.write(content.data)
-    return True
+    return "Successfully"
 
 def list():
     fileList = os.listdir()
     return fileList
 
 def main():
-    host = "127.0.1.1"
+    host = "192.168.127.103"
     port = 8080
     server = xmlrpc.server.SimpleXMLRPCServer((host, port), allow_none=True)
     print("Start the server")
     server.register_function(upload, "upload")
     server.register_function(list, "list")
-    server.register_function(upload, "upload")
+    server.register_function(download, "download")
     server.serve_forever()
 
     return 
