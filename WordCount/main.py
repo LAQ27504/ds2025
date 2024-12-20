784231fee6ccd0ec22b05dc7451ecfb552720a44
LAQ27504 <anhquangcsp@gmail.com>
2024-12-20 16:53:08 +0700

    Word count system with MPI

--- WordCount/main.py
+++ WordCount/main.py
@@ -1,24 +1,84 @@
 from mpi4py import MPI
 import mmh3
+from functools import reduce
  
 comm = MPI.COMM_WORLD 
 rank = comm.Get_rank()
+size = comm.Get_size()
 
-def map(data_chunks):
-   text = data_chunks[:-1].split(" ")
+workers = [i for i in range(size)]
+
+re_map = []
+
+def mapper(data_chunks):
+   text = data_chunks.split(" ")
    mapping = [(i, 1) for i in text]
-   for i in mapping:
-      reducer_id = partition(i[0])
-      print((i[0],reducer_id))
-      
-def partition(word):
-   return (mmh3.hash(word) % 3) + 1
+   partition(mapping=mapping)
 
 
-if rank > 1: 
-   filePath = f"text{rank - 1}.txt"
+def partition(mapping):
+   for i, pair in enumerate(mapping):
+      word = pair[0]
+      parti_id = (mmh3.hash(word) % 3) 
+      if (parti_id != rank):
+         print(f"Sending word {word} from {rank} to {parti_id}")
+         comm.send(pair, dest=parti_id)
+      else: 
+         print(f"Rank {rank} re-append {word}")
+         re_map.append(pair)
+      if i == len(mapping) - 1:
+         for j in workers:
+            if (j == rank):
+               continue
+            print(f"Rank {rank} sending 1 to {j}")
+            comm.send(1, dest=j)
 
-   with open(filePath, "r") as f:
-      map(f.read())
+   for i in workers:
+      if (i == rank):
+         continue
+      running = True
+      while running:
+         data = comm.recv(source=i)
+         if data != 1:
+            re_map.append(data)
+            continue
+         running = False
+   shuffle()
 
+def get_word(e):
+   return e[0]
 
+def shuffle():
+   re_map.sort(key=get_word)
+   print(re_map)
+
+def accumulate_counts(acc, item):
+   word, count = item 
+   acc[word] = acc.get(word, 0) + count
+   return acc
+
+def reducer():
+   word_count = reduce(accumulate_counts, re_map, {})
+   print(word_count)
+   return word_count
+
+def remove_symbol(text:str):
+   symbol = ".?,!@#$%^&*()_+=[]"
+   word_without_sym = str.maketrans("-"," ",symbol)
+   text = text.translate(word_without_sym)
+   text = text.replace("\n", " ")
+   return text
+
+with open("WordCount.txt", "w") as f:
+   pass
+print(f"This is rank: {rank}")
+filePath = f"text{rank + 1}.txt"
+word_count = []
+with open(filePath, "r") as f:
+   mapper(remove_symbol(f.read()))
+   word_count = reducer()
+
+with open("WordCount.txt", "a") as f:
+   for i in word_count:
+      text = f"{i} : {word_count[i]}\n"
+      f.write(text)
--- WordCount/test.py
+++ WordCount/test.py
@@ -1,11 +1,2 @@
-from mpi4py import MPI 
-
-comm = MPI.COMM_WORLD 
-rank = comm.Get_rank() 
-
-
-data = 0
-if rank == 0:
-   comm.bcast("abc", root=0) 
-else: 
-   print(comm.bcast("", root=0))
+from mmh3 import hash
+print(hash(2))
